9bc6c0048383c5501b12771479c359c7eba04ed9
snepal01 <snepal92@gmail.com>
2016-10-20 17:00:09 -0400

    Minor changes

--- main.cpp
+++ main.cpp
@@ -8,33 +8,14 @@ using namespace Tins;
 void random_string(char *s);
 int main() 
 {
-    // NetworkInterface iface = NetworkInterface::default_interface();
-    
-   
-    // NetworkInterface::Info info = iface.addresses();
-    
-   
-    // EthernetII eth("77:22:33:11:ad:ad", info.hw_addr);
-   
-    // eth /= IP("192.168.0.1", info.ip_addr);
-    
-   
-    // eth /= TCP(13, 15);
-   
-    // eth /= RawPDU("I'm a payload!");
-   
-    // PacketSender sender;
-   
-    // sender.send(eth, iface);
-
     PacketSender sender;
 	char *s = new char [1206];
     int rand_no;
-	while(1){	
-		
+    NetworkInterface iface("eth10");
+	while(1){		
 		random_string(s);
 		EthernetII pkt = EthernetII() / IP("255.255.255.255") / UDP() / RawPDU(s);
-		sender.send(pkt, "eth10"); // send it through eth0
+		sender.send(pkt, iface); // send it through eth0
 		usleep(400);
 	}
 
