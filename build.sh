76c734c6476cf543b7815cb506900f60ca6f6b9d
snepal01 <snepal92@gmail.com>
2016-11-02 15:42:16 -0400

    Shell script added

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+#export portName="eth10"
+virtual_ethernet()
+{
+	echo "setting virtual ethernet port"
+	sudo modprobe dummy
+	lsmod | grep dummy
+
+	if (sudo ip link set name eth10 dev dummy0 2> /dev/null); then
+		echo "succesfully created a virtual ethernet"
+		sudo ifconfig eth10 up 
+	else 
+		echo "error in creating vitual ethernet port"
+		echo -n "Do you want to clean up existing eth10 [Y/n]"
+		sudo ifconfig eth10 up 
+		read character
+		case $character in
+			Y)
+				echo "cleaning up existing device"
+				sudo ip link delete eth10 type dummy
+				sudo rmmod dummy
+				;;
+			n)
+				exit 1
+				;;
+		esac
+	fi
+}
+
+
+LSUSBINFO="$(lsusb | grep PEAK | cut -d' ' -f 6)"
+
+virtual_ethernet
